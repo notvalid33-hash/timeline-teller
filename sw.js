0f1e3d1593ea5bfc16969cf5753b2e7de6f3a64d
notvalid33-hash <notvalid33@gmail.com>
2026-09-18 01:48:31 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tt-v2-2.0.0';
+const CACHE='tt-v2-2.1.1';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('tt-v')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
